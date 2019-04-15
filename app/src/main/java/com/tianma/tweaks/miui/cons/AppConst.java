a23be3459c21b3f9e6e92b8c8519d430da4ceb27
tianma8023 <1808931265@qq.com>
2019-04-16 00:19:27 +0800

    [Others] Provide correct source code site.

--- app/src/main/java/com/tianma/tweaks/miui/cons/AppConst.java
+++ app/src/main/java/com/tianma/tweaks/miui/cons/AppConst.java
@@ -6,6 +6,6 @@ public interface AppConst {
 
     String MAIN_ACTIVITY_ALIAS = BuildConfig.APPLICATION_ID + ".app.MainActivityAlias";
 
-    String PROJECT_SOURCE_CODE_URL = "https://github.com/tianma8023/XposedSmsCode";
+    String PROJECT_SOURCE_CODE_URL = "https://github.com/tianma8023/MiTweaks";
 
 }
