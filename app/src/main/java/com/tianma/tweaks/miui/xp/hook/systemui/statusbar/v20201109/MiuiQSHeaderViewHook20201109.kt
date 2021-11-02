84a766dddaa783593f7d485f24c342a66b227a46
tianma8023 <1808931265@qq.com>
2021-11-02 21:07:01 +0800

    [refactor][plugin] Migrate from java to kotlin.

--- app/src/main/java/com/tianma/tweaks/miui/xp/hook/systemui/statusbar/v20201109/MiuiQSHeaderViewHook20201109.kt
+++ app/src/main/java/com/tianma/tweaks/miui/xp/hook/systemui/statusbar/v20201109/MiuiQSHeaderViewHook20201109.kt
@@ -1,154 +1,154 @@
-package com.tianma.tweaks.miui.xp.hook.systemui.statusbar.v20201109;
+package com.tianma.tweaks.miui.xp.hook.systemui.statusbar.v20201109
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import com.tianma.tweaks.miui.BuildConfig;
-import com.tianma.tweaks.miui.data.sp.XPrefContainer;
-import com.tianma.tweaks.miui.utils.ResolutionUtils;
-import com.tianma.tweaks.miui.utils.XLogKt;
-import com.tianma.tweaks.miui.xp.hook.BaseSubHook;
-import com.tianma.tweaks.miui.xp.hook.systemui.helper.ResHelpers;
-import com.tianma.tweaks.miui.xp.hook.systemui.weather.WeatherMonitor;
-import com.tianma.tweaks.miui.xp.hook.systemui.weather.WeatherObserver;
-import com.tianma.tweaks.miui.xp.utils.appinfo.AppInfo;
-import com.tianma.tweaks.miui.xp.wrapper.MethodHookWrapper;
-import com.tianma.tweaks.miui.xp.wrapper.XposedWrapper;
+import android.content.Context
+import android.content.pm.PackageManager
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.tianma.tweaks.miui.BuildConfig
+import com.tianma.tweaks.miui.data.sp.XPrefContainer
+import com.tianma.tweaks.miui.utils.ResolutionUtils
+import com.tianma.tweaks.miui.utils.logD
+import com.tianma.tweaks.miui.xp.hook.BaseSubHook
+import com.tianma.tweaks.miui.xp.hook.systemui.helper.ResHelpers
+import com.tianma.tweaks.miui.xp.hook.systemui.weather.WeatherMonitor
+import com.tianma.tweaks.miui.xp.hook.systemui.weather.WeatherObserver
+import com.tianma.tweaks.miui.xp.utils.appinfo.AppInfo
+import com.tianma.tweaks.miui.xp.wrapper.MethodHookWrapper
+import com.tianma.tweaks.miui.xp.wrapper.XposedWrapper
 
 /**
  * 下拉状态栏头部View Hook（下拉状态栏显示天气等)，适用版本 MiuiSystemUI(versionCode>=202011090)
  */
-public class MiuiQSHeaderViewHook20201109 extends BaseSubHook implements WeatherObserver {
+class MiuiQSHeaderViewHook20201109(classLoader: ClassLoader?, appInfo: AppInfo?) :
+    BaseSubHook(classLoader, appInfo), WeatherObserver {
 
-    private static final String CLASS_HEADER_VIEW = "com.android.systemui.qs.MiuiQSHeaderView";
+    private var mHeaderViewClass: Class<*>? = null
 
-    private Class<?> mHeaderViewClass;
+    private var mModContext: Context? = null
+    private var mAppContext: Context? = null
 
-    private Context mModContext;
-    private Context mAppContext;
+    private var mWeatherInfoTv: TextView? = null
 
-    private TextView mWeatherInfoTv;
+    private val mWeatherEnabled: Boolean = XPrefContainer.isDropdownStatusBarWeatherEnabled
+    private var mWeatherTextColor = 0
+    private var mWeatherTextSize = 0f
 
-    private final boolean mWeatherEnabled;
-    private int mWeatherTextColor;
-    private float mWeatherTextSize;
+    companion object {
+        private const val CLASS_HEADER_VIEW = "com.android.systemui.qs.MiuiQSHeaderView"
+    }
 
-    public MiuiQSHeaderViewHook20201109(ClassLoader classLoader, AppInfo appInfo) {
-        super(classLoader, appInfo);
-
-        mWeatherEnabled = XPrefContainer.isDropdownStatusBarWeatherEnabled();
+    init {
         if (mWeatherEnabled) {
-            mWeatherTextColor = XPrefContainer.getDropdownStatusBarWeatherTextColor();
-            mWeatherTextSize = XPrefContainer.getDropdownStatusBarWeatherTextSize();
+            mWeatherTextColor = XPrefContainer.dropdownStatusBarWeatherTextColor
+            mWeatherTextSize = XPrefContainer.getDropdownStatusBarWeatherTextSize()
         }
     }
 
-    @Override
-    public void startHook() {
-        XLogKt.logD("Hooking MiuiQSHeaderView...");
+    override fun startHook() {
+        logD("Hooking MiuiQSHeaderView...")
         if (mWeatherEnabled) {
-            mHeaderViewClass = XposedWrapper.findClass(CLASS_HEADER_VIEW, getMClassLoader());
+            mHeaderViewClass = XposedWrapper.findClass(CLASS_HEADER_VIEW, mClassLoader)
             if (mHeaderViewClass != null) {
-                hookConstructor();
-                hookOnFinishInflate();
-                hookOnAttachedToWindow();
-                hookOnDetachedFromWindow();
+                hookConstructor()
+                hookOnFinishInflate()
+                hookOnAttachedToWindow()
+                hookOnDetachedFromWindow()
             }
         }
     }
 
     // HeaderView#constructor()
-    private void hookConstructor() {
+    private fun hookConstructor() {
         XposedWrapper.hookAllConstructors(mHeaderViewClass,
-                new MethodHookWrapper() {
-                    @Override
-                    protected void after(MethodHookParam param) {
-                        if (mAppContext == null) {
-                            View headerView = (View) param.thisObject;
-                            mModContext = headerView.getContext().getApplicationContext();
-                            try {
-                                mAppContext = mModContext
-                                        .createPackageContext(BuildConfig.APPLICATION_ID, Context.CONTEXT_IGNORE_SECURITY);
-                            } catch (PackageManager.NameNotFoundException e) {
-                                // ignore
-                            }
+            object : MethodHookWrapper() {
+                override fun after(param: MethodHookParam) {
+                    if (mAppContext == null) {
+                        val headerView = param.thisObject as View
+                        mModContext = headerView.context.applicationContext
+                        try {
+                            mAppContext = mModContext?.createPackageContext(
+                                BuildConfig.APPLICATION_ID,
+                                Context.CONTEXT_IGNORE_SECURITY
+                            )
+                        } catch (e: PackageManager.NameNotFoundException) {
+                            // ignore
                         }
                     }
-                });
+                }
+            })
     }
 
     // HeaderView#onFinishInflate()
-    private void hookOnFinishInflate() {
+    private fun hookOnFinishInflate() {
         XposedWrapper.findAndHookMethod(mHeaderViewClass,
-                "onFinishInflate",
-                new MethodHookWrapper() {
-                    @Override
-                    protected void after(MethodHookParam param) {
-                        // MiuiQSHeaderView extends RelativeLayout
-                        ViewGroup miuiQSHeaderView = (ViewGroup) param.thisObject;
+            "onFinishInflate",
+            object : MethodHookWrapper() {
+                override fun after(param: MethodHookParam) {
+                    // MiuiQSHeaderView extends RelativeLayout
+                    val miuiQSHeaderView = param.thisObject as ViewGroup
 
-                        // 右上角齿轮快捷按钮
-                        Context context = miuiQSHeaderView.getContext();
-                        int shortcutViewId = ResHelpers.getId(context.getResources(), "notification_shade_shortcut");
-                        ImageView shortcutView = (ImageView) miuiQSHeaderView.findViewById(shortcutViewId);
+                    // 右上角齿轮快捷按钮
+                    val context = miuiQSHeaderView.context
+                    val shortcutViewId =
+                        ResHelpers.getId(context.resources, "notification_shade_shortcut")
+                    val shortcutView = miuiQSHeaderView.findViewById<View>(shortcutViewId)
 
-                        mWeatherInfoTv = new TextView(miuiQSHeaderView.getContext());
-                        RelativeLayout.LayoutParams weatherInfoLp = new RelativeLayout.LayoutParams(
-                                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                                RelativeLayout.LayoutParams.WRAP_CONTENT
-                        );
-                        weatherInfoLp.addRule(RelativeLayout.START_OF, shortcutView.getId());
-                        weatherInfoLp.addRule(RelativeLayout.ALIGN_TOP, shortcutView.getId());
-                        weatherInfoLp.addRule(RelativeLayout.ALIGN_BOTTOM, shortcutView.getId());
-                        weatherInfoLp.setMarginEnd((int) ResolutionUtils.dp2px(miuiQSHeaderView.getContext(), 3));
-                        mWeatherInfoTv.setLayoutParams(weatherInfoLp);
-
-                        mWeatherInfoTv.setIncludeFontPadding(false);
-                        mWeatherInfoTv.setGravity(Gravity.CENTER);
-
-                        mWeatherInfoTv.setTextColor(mWeatherTextColor);
-                        mWeatherInfoTv.setTextSize(mWeatherTextSize);
-
-                        miuiQSHeaderView.addView(mWeatherInfoTv);
+                    val weatherInfoLp = RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                    ).also {
+                        it.addRule(RelativeLayout.START_OF, shortcutView.id)
+                        it.addRule(RelativeLayout.ALIGN_TOP, shortcutView.id)
+                        it.addRule(RelativeLayout.ALIGN_BOTTOM, shortcutView.id)
+                        it.marginEnd =
+                            ResolutionUtils.dp2px(miuiQSHeaderView.context, 3f).toInt()
                     }
-                });
+
+                    mWeatherInfoTv = TextView(miuiQSHeaderView.context).also {
+                        it.includeFontPadding = false
+                        it.gravity = Gravity.CENTER
+                        it.setTextColor(mWeatherTextColor)
+                        it.textSize = mWeatherTextSize
+                        it.layoutParams = weatherInfoLp
+                    }
+
+                    miuiQSHeaderView.addView(mWeatherInfoTv)
+                }
+            })
     }
 
     // HeaderView#onAttachedToWindow()
-    private void hookOnAttachedToWindow() {
+    private fun hookOnAttachedToWindow() {
         XposedWrapper.findAndHookMethod(mHeaderViewClass,
-                "onAttachedToWindow",
-                new MethodHookWrapper() {
-                    @Override
-                    protected void after(MethodHookParam param) {
-                        WeatherMonitor.get(mAppContext).registerObserver(MiuiQSHeaderViewHook20201109.this);
-                    }
-                });
+            "onAttachedToWindow",
+            object : MethodHookWrapper() {
+                override fun after(param: MethodHookParam) {
+                    val context = mAppContext ?: return
+                    WeatherMonitor.get(context)
+                        .registerObserver(this@MiuiQSHeaderViewHook20201109)
+                }
+            })
     }
 
     // HeaderView#onDetachedFromWindow()
-    private void hookOnDetachedFromWindow() {
+    private fun hookOnDetachedFromWindow() {
         XposedWrapper.findAndHookMethod(mHeaderViewClass,
-                "onDetachedFromWindow",
-                new MethodHookWrapper() {
-                    @Override
-                    protected void after(MethodHookParam param) {
-                        WeatherMonitor.get(mAppContext).unregisterObserver(MiuiQSHeaderViewHook20201109.this);
-                    }
-                });
+            "onDetachedFromWindow",
+            object : MethodHookWrapper() {
+                override fun after(param: MethodHookParam) {
+                    val context = mAppContext ?: return
+                    WeatherMonitor.get(context)
+                        .unregisterObserver(this@MiuiQSHeaderViewHook20201109)
+                }
+            })
     }
 
-    @Override
-    public void onWeatherChanged(String newWeatherInfo) {
-        XLogKt.logD("onWeatherChanged: %s", newWeatherInfo);
-        if (mWeatherInfoTv != null) {
-            mWeatherInfoTv.setText(newWeatherInfo);
-        }
+    override fun onWeatherChanged(newWeatherInfo: String) {
+        logD("onWeatherChanged: %s", newWeatherInfo)
+        mWeatherInfoTv?.text = newWeatherInfo
+
     }
 }
